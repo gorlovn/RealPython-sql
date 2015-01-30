b1062c92dcc7c0210af70535a3cbbea9d847a813
Nikolay Gorlov <gnv@ctmed.ru>
2015-01-30 13:46:22 +0600

    6.1 - 6.2 of RealPython Part2

---
+++ sql-2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf8
+# Create a SQLite3 database and table
+
+# import sqlite3 library
+import sqlite3
+
+# create a new database if the database doesn't already exist
+conn = sqlite3.connect("cars.db")
+
+# get a cursor object used to execute SQL commands
+cursor = conn.cursor()
+
+# create a table
+cursor.execute("""CREATE TABLE inventory 
+                (make TEXT, model TEXT, qty INT)
+                """)
+
+# close the database connection
+conn.close()
